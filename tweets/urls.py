70f0f241a1d29149e83cea11317b164542a8da41
Clyde <andreicunananml14@gmail.com>
2025-08-18 22:26:45 +0800

    class based view - no dynamic url

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', tweet_list_view, name='list_view'),
-    path('1/', tweet_detail_view, name='detail_view'),
+    path('', TweetListView.as_view(), name='list_view'),
+    path('1/', TweetDetailView.as_view(), name='detail_view'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 from .models import Tweet
 
-def tweet_detail_view(request, id=1):
-    obj = Tweet.objects.get(id=id)
-    print(obj)
-    context = {
-        'object' : obj,
-    #     assigning html element to python element
-    }
-    return render(request, 'detail_view.html' , context )
+class TweetDetailView(DetailView):
+    template_name = 'detail_view.html'
 
-def tweet_list_view(request):
-    qs = Tweet.objects.all()
-    print(qs)
-    context = {
-        'query' : qs,
-    }
-    return render(request, 'list_view.html' , context )
+    def get_object(self, queryset=Tweet.objects.all()):
+        return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+    template_name = 'list_view.html'
